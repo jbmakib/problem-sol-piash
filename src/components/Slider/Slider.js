6114ed52ca6805798216e1fc7449615bfc9f26e9
jbmakib <mdfoujolmobin@gmail.com>
2021-12-17 21:35:36 +0600

    updated

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -8,11 +8,30 @@ const Slider = ({ lowest, highest, value, setValue }) => {
         setValue(value);
     };
 
-    let addedValue = 0;
+    console.log(divide);
 
+    let addedValue = 0;
     if (highest <= 10) {
-        if (divide > 4) {
+        if (divide === Infinity || divide > 10) {
+            addedValue = 9;
+        } else if (divide >= 6.6666666666666667) {
             addedValue = 8;
+        } else if (divide >= 4) {
+            addedValue = 8;
+        } else if (divide >= 3) {
+            addedValue = 7;
+        } else if (divide >= 2.5) {
+            addedValue = 6;
+        } else if (divide >= 2) {
+            addedValue = 5;
+        } else if (divide >= 1.6666666666666667) {
+            addedValue = 4.25;
+        } else if (divide >= 1.4285714285714286) {
+            addedValue = 3.5;
+        } else if (divide >= 1.25) {
+            addedValue = 2.5;
+        } else if (divide >= 0.5) {
+            addedValue = 1;
         }
     } else {
         if (divide === Infinity || divide >= 6.6666666666666667) {
